0b7acc02491fad173e38425cb78ada529efa0f78
swanchia <36022888+swanchia@users.noreply.github.com>
2020-08-13 19:07:17 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,162 @@
+var baseURL = 'https://cdn.jsdelivr.net/gh/swanchia/CDN/images/'
+var data = {
+    hot: [
+        {
+            name: '尹素婉',
+            view: '31589',
+            cover: baseURL + 'hot/play.gif'
+        },
+        {
+            name: '水幂桃',
+            view: '7572',
+            cover: baseURL + 'hot/1.jpg'
+        },
+        {
+            name: '温碗儿',
+            view: '6896',
+            cover: baseURL + 'hot/2.jpg'
+        },
+        {
+            name: '韩水水',
+            view: '6628',
+            cover: baseURL + 'hot/3.jpg'
+        },
+        {
+            name: '草莓妹',
+            view: '6034',
+            cover: baseURL + 'hot/4.gif'
+        },
+        {
+            name: '蜜豆baby',
+            view: '27582',
+            cover: baseURL + 'hot/5.jpg'
+        },      
+        {
+            name: '茜茜酱',
+            view: '5641',
+            cover: baseURL + 'hot/6.jpg'
+        },
+        {
+            name: '允儿',
+            view: '6896',
+            cover: baseURL + 'hot/7.jpg'
+        },
+        {
+            name: '朵妃',
+            view: '6628',
+            cover: baseURL + 'hot/8.jpg'
+        },
+        {
+            name: '花仙小倩',
+            view: '6034',
+            cover: baseURL + 'hot/9.jpg'
+        },
+        {
+            name: '依依',
+            view: '5641',
+            cover: baseURL + 'hot/10.jpg'
+        },
+        {
+            name: '炸柠檬',
+            view: '6896',
+            cover: baseURL + 'hot/11.jpg'
+        },
+        {
+            name: '丝丝',
+            view: '5641',
+            cover: baseURL + 'hot/12.jpg'
+        },
+        {
+            name: '主播北鼻',
+            view: '6628',
+            cover: baseURL + 'hot/13.jpg'
+        },
+        {
+            name: '丝丝',
+            view: '6034',
+            cover: baseURL + 'hot/14.jpg'
+        },    
+        {
+            name: '主播小北',
+            view: '5035',
+            cover: baseURL + 'hot/15.gif'
+        },
+        {
+            name: '曼曼',
+            view: '5035',
+            cover: baseURL + 'hot/16.jpg'
+        },
+        {
+            name: '黛蜜儿',
+            view: '5035',
+            cover: baseURL + 'hot/17.jpg'
+        },
+        {
+            name: '林包子',
+            view: '5035',
+            cover: baseURL + 'hot/18.jpg'
+        },
+        {
+            name: '萱宝宝',
+            view: '5035',
+            cover: baseURL + 'hot/19.jpg'
+        }
+     
+    ],
+    video: [
+        {
+            name: '曦嘻吸熙',
+            cover: baseURL + 'video/1.jpg'
+        },
+        {
+            name: '路西',
+            cover: baseURL + 'video/2.jpg'
+        },
+        {
+            name: '哈妮宝贝',
+            cover: baseURL + 'video/3.jpg'
+        },
+        {
+            name: '梦老师',
+            cover: baseURL + 'video/4.jpg'
+        },
+        {
+            name: '艾玛寒亚',
+            view: '5641',
+            cover: baseURL + 'video/5.jpg'
+        },
+        {
+            name: '七猫猫',
+            view: '5641',
+            cover: baseURL + 'video/6.jpg'
+        },
+        {
+            name: '咪咪酱',
+            view: '5641',
+            cover: baseURL + 'video/13.jpg'
+        },
+        {
+            name: '妲仙',
+            view: '5641',
+            cover: baseURL + 'video/8.jpg'
+        },
+        {
+            name: '水蜜桃',
+            cover: baseURL + 'video/9.jpg'
+        },
+        {
+            name: '梦梵儿',
+            view: '5641',
+            cover: baseURL + 'video/10.jpg'
+        },
+        {
+            name: '养乐多',
+            view: '5641',
+            cover: baseURL + 'video/11.jpg'
+        },      
+        {
+            name: '艾菲尔',
+            cover: baseURL + 'video/12 (1).jpg'
+        }
+    ]
+}
